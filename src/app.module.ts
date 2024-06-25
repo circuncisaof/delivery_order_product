9f332ca9cc733b35f8ee56561168851d2177af45
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-25 16:54:55 -0300

    install package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderEntity } from './orders/entities/order.entity';
+import { Order } from './orders/order.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mari0001',
+      database: 'postgres',
+      entities: [OrderEntity],
+      synchronize: true,
+    }),
+    Order,
+  ],
   controllers: [],
   providers: [],
 })
